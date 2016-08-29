054e7f24208bbbbcbc5594ea83a530a0b3155822
Steve Miner <steveminer@gmail.com>
2016-08-29 15:36:50 -0400

    prep release

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject com.velisco/strgen "0.1.0-SNAPSHOT"
-  :description "String generator for use with Clojure test.check and spec"
+(defproject com.velisco/strgen "0.1.1"
+  :description "String generator from regular expressions, for use with Clojure test.check and spec"
   :url "https://github.com/miner/strgen"
+  :deploy-repositories {"releases" :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
