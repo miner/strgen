09f2a37f4cf3f535a61f854473b4cb1e029fe486
Steve Miner <steveminer@gmail.com>
2016-09-29 12:59:35 -0400

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.velisco/strgen "0.1.1"
+(defproject com.velisco/strgen "0.1.2"
   :description "String generator from regular expressions, for use with Clojure test.check and spec"
   :url "https://github.com/miner/strgen"
   :deploy-repositories {"releases" :clojars}
