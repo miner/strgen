6653e9b5e78f02093cf4b4a54454ebc899648fa3
Steve Miner <steveminer@gmail.com>
2017-09-18 15:53:42 -0400

    adopt Clojure 1.9-beta1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :deploy-repositories {"releases" :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [org.clojure/test.check "0.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
+                 [org.clojure/test.check "0.10.0-alpha2"]])
 
 
