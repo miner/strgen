6c36f010a2514bb37c9070b4bfde328abfe3f435
mikeyjcat <mike@hewitson.co.za>
2018-01-15 11:55:38 +0200

    bumpt clojure dependency to 1.9

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :deploy-repositories {"releases" :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/test.check "0.9.0"]])
 
 
