09a6c4f396da0e4eb8d27ee6ec222fdd8823e0fd
Steve Miner <steveminer@gmail.com>
2020-02-19 14:47:20 -0500

    lib updates

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.velisco/strgen "0.1.8"
+(defproject com.velisco/strgen "0.1.9"
   :description "String generator from regular expressions, for use with Clojure test.check and spec"
   :url "https://github.com/miner/strgen"
   :deploy-repositories {"releases" :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.339"]
-                 [org.clojure/test.check "0.9.0"]])
+                 [org.clojure/clojurescript "1.10.439"]
+                 [org.clojure/test.check "1.0.0"]])
 
