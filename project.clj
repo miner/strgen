3032c21b0bfa094d1261c58aff769b1fe46a1068
Steve Miner <steveminer@gmail.com>
2021-04-11 07:39:23 -0400

    revert spec

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.velisco/strgen "0.2.1"
+(defproject com.velisco/strgen "0.2.2"
   :description "String generator from regular expressions, for use with Clojure test.check and spec"
   :url "https://github.com/miner/strgen"
   :deploy-repositories {"releases" :clojars}
@@ -6,6 +6,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/clojurescript "1.10.844"]
-                 [org.clojure/spec.alpha "0.2.194"]
+                 ;;[org.clojure/spec.alpha "0.2.194"]
                  [org.clojure/test.check "1.1.0"]])
 
