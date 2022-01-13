0629f1124576cdef92da6c2ee7f09d39f73869e5
Steve Miner <steveminer@gmail.com>
2022-01-13 18:16:18 -0500

    libs

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject com.velisco/strgen "0.2.2"
+(defproject com.velisco/strgen "0.2.3"
   :description "String generator from regular expressions, for use with Clojure test.check and spec"
   :url "https://github.com/miner/strgen"
   :deploy-repositories {"releases" :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/clojurescript "1.10.844"]
+                 [org.clojure/clojurescript "1.11.4"]
                  ;;[org.clojure/spec.alpha "0.2.194"]
-                 [org.clojure/test.check "1.1.0"]]
+                 [org.clojure/test.check "1.1.1"]]
   :repositories {"sonatype-snapshot"
                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}})
 
