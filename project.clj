6d0521989b36d8a3abe6d41612da1606d9467920
Steve Miner <steveminer@gmail.com>
2023-11-14 16:12:15 -0500

    dep

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.velisco/strgen "0.2.4"
+(defproject com.velisco/strgen "0.2.5"
   :description "String generator from regular expressions, for use with Clojure test.check and spec"
   :url "https://github.com/miner/strgen"
   :deploy-repositories {"releases" :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/clojurescript "1.11.4"]
                  [org.clojure/test.check "1.1.1"]]
   )
